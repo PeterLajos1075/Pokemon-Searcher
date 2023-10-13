427eaca77dca0337ee1eff73216ef81e8bb31dfa
Kovács <PETER.KOVACS4@HAUNI.COM>
2023-10-13 10:44:12 +0200

    commit

--- src/Project/SearchPage/NoData.tsx
+++ src/Project/SearchPage/NoData.tsx
@@ -3,13 +3,21 @@ const NoData = () => {
 
     return(
         <div className ="NoData">
-            <Text>
+            <Text
+                style={{
+                    fontSize:"30px",
+                    justifyContent:"center",
+                }}
+            >
                 A Pokemon nem található
             </Text>
             <Text
-            style={{}}
+            style={{
+                fontSize:"20px",
+                justifyContent:"center",
+            }}
             >
-            Keress rá egy Pokemonra a kereső segítségével
+                Keress rá egy Pokemonra a kereső segítségével
             </Text>
             <img alt="logo" src="https://cdn141.picsart.com/316822369071211.png" style={{width: '80%'}}/>
         </div>
